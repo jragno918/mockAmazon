f55f7e3c31c538fe7fd9ea98b4dadbd78e6e7fee
Joe Ragno <joseph.ragno3@gmail.com>
2018-07-31 09:56:25 -0400

    finished database setup with ten products

---
+++ amazonCustomer.js
@@ -0,0 +1,41 @@
+// Requires mysql to interact with the amazon_DB server
+var mysql = require("mysql");
+// Requires inquirer to interact with the CLI
+var inquirer = require("inquirer");
+
+// create the connection information for the sql database
+var connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "",
+  database: "amazon_DB"
+});
+
+// connect to the mysql server and sql database
+connection.connect(function(err) {
+  if (err) throw err;
+  // run the start function after the connection is made to prompt the user
+  start();
+});
+
+function start() {
+
+}
+
+function placeOrder() {
+
+
+
+
+}
+
+function quantityCheck() {
+  
+}
--- schema.sql
+++ schema.sql
@@ -7,7 +7,39 @@ CREATE TABLE products(
   item_id INT NOT NULL AUTO_INCREMENT,
   product_name VARCHAR(150) NOT NULL,
   department VARCHAR(50) NOT NULL,
-  price DECIMAL(10,4) NOT NULL,
+  price DECIMAL(10,2) NOT NULL,
   stock_quantity INT NOT NULL,
   PRIMARY KEY(item_id)
 );
+
+SELECT * FROM products;
+
+INSERT INTO products(item_id, product_name, department, price, stock_quantity)
+VALUES (2375, 'Samsung 50\" 4K UHD Smart TV', 'Electronics', 439.99, 10);
+
+INSERT INTO products(item_id, product_name, department, price, stock_quantity)
+VALUES (6495, '15\" MacBook Pro', 'Electronics', 2200.99, 22);
+
+INSERT INTO products(item_id, product_name, department, price, stock_quantity)
+VALUES (1780, 'Men\'s Dress Shoes', 'Apperal', 35.47, 25);
+
+INSERT INTO products(item_id, product_name, department, price, stock_quantity)
+VALUES (3549, 'KitchenAid Mixer', 'Kitchen Appliances', 350.00, 10);
+
+INSERT INTO products(item_id, product_name, department, price, stock_quantity)
+VALUES (9341, 'Wool Sweater', 'Apperal', 45.23, 50);
+
+INSERT INTO products(item_id, product_name, department, price, stock_quantity)
+VALUES (8335, 'Hair Straightener', 'Beauty and Cosmetics', 75.47, 15);
+
+INSERT INTO products(item_id, product_name, department, price, stock_quantity)
+VALUES (0183, 'Grill Cover', 'Outdoors', 65.33, 50);
+
+INSERT INTO products(item_id, product_name, department, price, stock_quantity)
+VALUES (8243, 'XBox One X', 'Electronics', 399.99, 20);
+
+INSERT INTO products(item_id, product_name, department, price, stock_quantity)
+VALUES (7256, 'Playstation 4 Pro', 'Electronics', 499.99, 25);
+
+INSERT INTO products(item_id, product_name, department, price, stock_quantity)
+VALUES (8339, 'Single Subject Notebook', 'School Supplies', 1.99, 100);
